c25f74718117bfdc6df0e67224716e001efce9e3
Luis Grizzo <Luis Grizzo>
2023-10-14 11:04:50 -0300

    chore: new configs to the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,32 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  safelist: [
+    {
+      pattern: /(bg|text|border|fill)-(red|yellow|lime|blue|pink)-(600|900)/,
+      variants: ['hover', 'focus', 'before']
+    }
+  ],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        xsm: '320px'
+      },
+      colors: {
+        background_color_lightTheme: '#FAFAFA',
+        shape_color_lightTheme: '#EEEEEE',
+        text_color_lightTheme: '#616161',
+        contrast_color_lightTheme: '#212121',
+
+        background_color_darkTheme: '#050505',
+        shape_color_darkTheme: '#111111',
+        text_color_darkTheme: '#9E9E9E',
+        contrast_color_darkTheme: '#DEDEDE'
+      }
+    }
   },
   plugins: []
 }
