69705caf2f49ada16e8d696cf8ca716bdd5026e7
Luis Grizzo <Luis Grizzo>
2024-05-01 02:53:16 -0300

    feat: add github and unsplash services

---
+++ src/services/github.ts
@@ -0,0 +1,22 @@
+import { cache } from 'react'
+import { Octokit } from '@octokit/core'
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_KEY
+})
+
+export const getProjects = cache(async () => {
+  const { status, data } = await octokit.request('GET /user/repos', {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    },
+    type: 'owner',
+    sort: 'updated'
+  })
+
+  if (![200, 304].includes(status)) {
+    throw new Error('Failed to fetch projects.')
+  }
+
+  return data
+})
