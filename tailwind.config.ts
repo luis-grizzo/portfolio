d841f193ff0376f117da5195705f3ef26e79a9d6
Luis Grizzo <Luis Grizzo>
2024-05-01 02:56:58 -0300

    chore: major refactor on project configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,29 +3,13 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
-  safelist: [
-    {
-      pattern: /(bg|text|border|fill)-(red|yellow|lime|blue|pink)-(600|900)/,
-      variants: ['hover', 'focus', 'before']
-    }
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/utils/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      screens: {
-        xsm: '320px'
-      },
-      colors: {
-        background_color_lightTheme: '#FAFAFA',
-        shape_color_lightTheme: '#EEEEEE',
-        text_color_lightTheme: '#616161',
-        contrast_color_lightTheme: '#212121',
-
-        background_color_darkTheme: '#050505',
-        shape_color_darkTheme: '#111111',
-        text_color_darkTheme: '#9E9E9E',
-        contrast_color_darkTheme: '#DEDEDE'
+      borderWidth: {
+        '1': '1px'
       }
     }
   },
