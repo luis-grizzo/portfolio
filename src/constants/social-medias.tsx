ed2eb2ff6a8cf3576acf13e7ccbd7f5e31279ee7
Luis Grizzo <Luis Grizzo>
2024-05-01 02:51:41 -0300

    feat: add new constants variables

---
+++ src/constants/social-medias.tsx
@@ -0,0 +1,25 @@
+import { FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa6'
+import { MdAlternateEmail } from 'react-icons/md'
+
+export const socialMedias = [
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/lu%C3%ADs-ot%C3%A1vio-gaido-grizzo-2a957a1b2/',
+    icon: <FaLinkedinIn />
+  },
+  {
+    name: 'Github',
+    url: 'https://github.com/luis-grizzo',
+    icon: <FaGithub />
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/luis_ozzirg/',
+    icon: <FaInstagram />
+  },
+  {
+    name: 'Email',
+    url: 'mailto:luisoggrizzo@gmail.com',
+    icon: <MdAlternateEmail />
+  }
+]
---
+++ src/constants/time.ts
@@ -0,0 +1,5 @@
+export const dayInSeconds = 60 * 60 * 24
+
+export const yearInMilliseconds = 1000 * 60 * 60 * 24 * 365
+
+export const halfYearInMilliseconds = 1000 * 60 * 60 * 24 * 180
