ac0bd2708860ee0aff0e7eaa6f5b880ff4bcd000
Luis Grizzo <luisoggrizzo@gmail.com>
2024-07-12 16:59:53 -0300

    feat: add new metadata infos and arts

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,13 @@
+import Image from 'next/image'
+
 import { AiOutlineLoading } from 'react-icons/ai'
 
-import { Logo } from '@/components/base'
+import logo from '@public/logo.svg'
 
 export default function Loading() {
   return (
     <main className="flex flex-col items-center justify-center gap-8 md:gap-8 lg:gap-16 h-[calc(100vh_-_114px_-_84px_-_1px)] container mx-auto mt-[114px] py-8 md:py-16 lg:py-32 px-4">
-      <Logo containerClassName="!h-16" />
+      <Image src={logo} alt="" className="w-16 aspect-square" />
 
       <AiOutlineLoading size={32} className="animate-spin" />
     </main>
