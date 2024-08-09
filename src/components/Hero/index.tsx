1cb7c2542c6211765299bd3fd828d5801329bcc5
Luis Grizzo <luisoggrizzo@gmail.com>
2024-08-09 16:09:37 -0300

    feat(hero): remove links icon

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image'
 import { motion, useScroll, useTransform } from 'framer-motion'
-import { GoLinkExternal } from 'react-icons/go'
 
 import type { Random } from 'unsplash-js/dist/methods/photos/types'
 
@@ -21,7 +20,7 @@ export function Hero({ image }: HeroProps) {
   const depth = useTransform(scrollYProgress, [0, 1], [1.2, 1])
 
   return (
-    <header className="relative flex flex-col items-center justify-center w-full h-[75vh] md:h-[70vh] lg:h-[65vh]  border-b-1 border-neutral-400/10">
+    <header className="relative flex flex-col items-center justify-center w-full h-[75vh] md:h-[70vh] lg:h-[65vh] border-b-1 border-neutral-400/10">
       <div className="container mx-auto px-4 py-8">
         <div className="flex flex-col justify-center gap-4 md:max-w-2xl lg:max-w-3xl xl:max-w-4xl">
           <motion.h1
@@ -63,27 +62,24 @@ export function Hero({ image }: HeroProps) {
         />
       </figure>
 
-      <span className="absolute bottom-8 flex items-center justify-center gap-1.5 w-full px-4 text-xs text-center text-pretty text-neutral-400">
-        {'Photo by'}
+      <span className="absolute bottom-8 px-4 text-xs text-center text-pretty text-neutral-300">
+        {'Photo by '}
         <a
           href={`https://unsplash.com/@${image.user.username}?utm_source=Unconscious&utm_medium=referral`}
           target="_blank"
           rel="noreferrer"
-          className="flex items-center gap-1 text-blue-400 hover:text-blue-200 transition-colors"
+          className="underline underline-offset-2 text-blue-400 hover:text-blue-200 transition-colors"
         >
           {image.user.name}
-
-          <GoLinkExternal className="h-3 w-3" />
         </a>
-        {'on'}
+        {' on '}
         <a
           href="https://unsplash.com/?utm_source=Unconscious&utm_medium=referral"
           target="_blank"
           rel="noreferrer"
-          className="flex items-center gap-1 text-blue-400 hover:text-blue-200 transition-colors"
+          className="underline underline-offset-2 text-blue-400 hover:text-blue-200 transition-colors"
         >
           Unsplash
-          <GoLinkExternal className="h-3 w-3" />
         </a>
       </span>
     </header>
